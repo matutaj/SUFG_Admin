1aa3fb33f5a725d6cd9c10d6cfd8ac201252aea1
matutaj <matutajorge2002@gmail.com>
2025-01-13 15:45:18 -0300

    feat: add modal

--- src/components/sections/dashboard/ProdutoTable.tsx
+++ src/components/sections/dashboard/ProdutoTable.tsx
@@ -1,11 +1,28 @@
+import * as React from 'react';
 import { Box, Button, Paper, Stack, Typography } from '@mui/material';
 import { DataGrid, useGridApiRef } from '@mui/x-data-grid';
 import IconifyIcon from 'components/base/IconifyIcon';
 import SearchFilter from 'components/common/SearchFilter';
 import CustomPagination from 'components/common/CustomPagination';
 import { columns, rows } from 'data/dashboard/produtoTableData';
+import Modal from '@mui/material/Modal';
 
-const ProdutoTable = () => {
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 1080,
+  height: 500,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function ProdutoTable() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   const apiRef = useGridApiRef();
 
   return (
@@ -35,9 +52,25 @@ const ProdutoTable = () => {
             borderRadius: 1.5,
           })}
           startIcon={<IconifyIcon icon="heroicons-solid:plus" />}
+          onClick={handleOpen}
         >
           <Typography variant="body2">Adiciona</Typography>
         </Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Modal>
       </Stack>
 
       <Box
@@ -63,6 +96,4 @@ const ProdutoTable = () => {
       <CustomPagination apiRef={apiRef} />
     </Paper>
   );
-};
-
-export default ProdutoTable;
+}
